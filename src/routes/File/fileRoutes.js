1106628e54e72d056c84da2d6df239677d5723fa
Rehman078 <rehmannaveed715@gmail.com>
2024-12-31 17:42:28 +0500

    lead and update contact and file module complete

--- src/routes/File/fileRoutes.js
+++ src/routes/File/fileRoutes.js
@@ -6,7 +6,7 @@ import {protect, authorizeRoles} from '../../middlewares/auth.js';
 const router = express.Router();
 router.post('/', protect, authorizeRoles("Admin", "Manager", "SalesRep"), upload.array('files', 10), fileController.createFile);
 router.get('/', protect, authorizeRoles("Admin", "Manager", "SalesRep"), fileController.getAllFiles);
-router.get('/:id', protect, authorizeRoles("Admin", "Manager", "SalesRep"), fileController.getFilesBySourceId);
+router.get('/:id', protect, authorizeRoles("Admin", "Manager",), fileController.getFilesBySourceId);
 router.put('/:id', protect, authorizeRoles("Admin", "Manager", "SalesRep"), upload.array('files', 10), fileController.updateFile);
 router.delete('/:id',protect, authorizeRoles("Admin", "Manager", "SalesRep"), fileController.deleteFiles);
 
