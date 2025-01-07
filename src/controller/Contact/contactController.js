3646777b28fdaf3be4bed69ca433bb0e8fbc64ea
Rehman078 <rehmannaveed715@gmail.com>
2025-01-07 17:09:30 +0500

    modify .env

--- src/controller/Contact/contactController.js
+++ src/controller/Contact/contactController.js
@@ -4,10 +4,10 @@ import { httpResponse } from "../../utils/index.js";
 import { fetchContactsByRole } from "../../utils/Contact/fetchContactByRole.js";
 import { deleteContactByRole } from "../../utils/Contact/deleteContactByRole.js";
 import { updateContactByRole } from "../../utils/Contact/updateContactByRole.js";
-import Assignment from "../../models/Contact/assignContactModel.js";
+import ContactAssignment from "../../models/Contact/assignContactModel.js";
 // CREATE Contact
 const createContact = async (req, res) => {
-  const { name, email, phone, address, company, tags} = req.body;
+  const { name, email, phone, address, company, tags } = req.body;
   const { error } = validateContact(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   try {
@@ -18,10 +18,14 @@ const createContact = async (req, res) => {
       address,
       company,
       tags,
-      created_by : req.user._id
+      created_by: req.user._id,
     });
     await newContact.save();
-    return httpResponse.CREATED(res, newContact, "Contact Created Successfully");
+    return httpResponse.CREATED(
+      res,
+      newContact,
+      "Contact Created Successfully"
+    );
   } catch (err) {
     return httpResponse.BAD_REQUEST(res, err);
   }
@@ -30,29 +34,41 @@ const createContact = async (req, res) => {
 // READ Contacts on base of role
 const getAllContacts = async (req, res) => {
   try {
-    const { role, _id } = req.user; 
-   return await fetchContactsByRole(role, _id, res);
+    const { role, _id } = req.user;
+    return await fetchContactsByRole(role, _id, res);
   } catch (err) {
     return httpResponse.BAD_REQUEST(res, err.message);
   }
 };
 
 // UPDATE Contact
-
-
 const updateContact = async (req, res) => {
   const id = req.params.id;
   const { name, email, phone, address, company, tags, salerep_Ids } = req.body;
   const { _id: userId, role } = req.user;
-  
-  const updatedData = { name, email, phone, address, company, tags, updated_by: userId };
+
+  const updatedData = {
+    name,
+    email,
+    phone,
+    address,
+    company,
+    tags,
+    updated_by: userId,
+  };
   try {
-    return await updateContactByRole(id, updatedData, userId, role, salerep_Ids, res);
+    return await updateContactByRole(
+      id,
+      updatedData,
+      userId,
+      role,
+      salerep_Ids,
+      res
+    );
   } catch (error) {
     return httpResponse.BAD_REQUEST(res, error.message);
   }
 };
-
 
 // DELETE Contact
 const deleteContact = async (req, res) => {
@@ -67,15 +83,15 @@ const deleteContact = async (req, res) => {
 
 //asigned salerep
 const assignContact = async (req, res) => {
-  const { _id } = req.user; 
-  const { salerep_ids } = req.body; 
+  const { _id } = req.user;
+  const { salerep_ids } = req.body;
   const { id: contact_id } = req.params;
 
   try {
     const contactExists = await Contact.findById(contact_id);
     console.log(contactExists);
     if (!contactExists) {
-      return res.status(404).json({ message: 'Contact not found' });
+      return res.status(404).json({ message: "Contact not found" });
     }
 
     // Create assignments for each SaleRep
@@ -84,24 +100,24 @@ const assignContact = async (req, res) => {
       salerep_id,
       assigned_by: _id,
     }));
-
-    // Save the assignments in bulk
-    await Assignment.insertMany(assignments);
-    // Optionally, fetch the updated data for response
-    const assignedReps = await Assignment.find({ contact_id }).populate('salerep_id');
-
-    return httpResponse.SUCCESS(res, assignedReps, "Contact assigned to sales representatives successfully");
+    await ContactAssignment.insertMany(assignments);
+    const assignedReps = await ContactAssignment.find({ contact_id }).populate(
+      "salerep_id"
+    );
+    return httpResponse.SUCCESS(
+      res,
+      assignedReps,
+      "Contact assigned to sales representatives successfully"
+    );
   } catch (err) {
     return httpResponse.BAD_REQUEST(res, err.message);
   }
 };
 
 export default {
-    createContact,
-    getAllContacts,
-    updateContact,
-    deleteContact,
-    assignContact
-
-}
-
+  createContact,
+  getAllContacts,
+  updateContact,
+  deleteContact,
+  assignContact,
+};
--- src/nodemailer/nodemailerIntegration.js
+++ src/nodemailer/nodemailerIntegration.js
@@ -1,20 +1,22 @@
 import nodemailer from "nodemailer";
-
+import dotenv from "dotenv";
+dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: "rehmanoffical078@gmail.com",
-    pass: "gynhrpstbqpocjva",
+    user: process.env.Email,
+    pass: process.env.Email_Pass,
   },
+
 });
 
 // async..await is not allowed in global scope, must use a wrapper
 async function sendMail(to, subject, html) {
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: 'rehmanoffical078@gmail.com', // sender address
+    from: process.env.Email, // sender address
     to,
     subject,
     html, // html body
